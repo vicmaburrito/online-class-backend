b035836ff4659ab27057973255e034071e62c926
Jhosep <jose-zromero@hotmail.com>
2023-03-03 11:43:09 -0600

    Add schedules controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'schedules/index'
+  get 'schedules/show'
+  get 'schedules/new'
+  get 'schedules/edit'
+  get 'schedules/create'
+  get 'schedules/update'
+  get 'schedules/destroy'
   get 'users/index'
   get 'users/show'
   get 'users/new'
