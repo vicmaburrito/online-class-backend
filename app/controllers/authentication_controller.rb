1ef530c772059d154f973aa1fbbcf529df2cd24c
vicma <vmaldaraca@gmail.com>
2023-03-06 16:20:02 -0600

    Add login method

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,14 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_user
+
+  def login
+    @user = User.find_by(email: params[:email])&.authenticate(params[:password])
+    if @user
+      token = jwt_encode({ user_id: @user.id })
+      time = Time.now + 24.hours.to_i
+      render json: { token: token , exp: time.strftime("%m-%d-%Y %H:%M"), username: @user.username }, status: :ok
+    else
+      render json: { errors: @user }, status: :unauthorized
+    end
+  end
+end
