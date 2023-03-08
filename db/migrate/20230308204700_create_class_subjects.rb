b22d63e56d197d813b00e0f27de577f6ec93b4b0
vicma <vmaldaraca@gmail.com>
2023-03-08 14:47:39 -0600

    Add class subject model

---
+++ db/migrate/20230308204700_create_class_subjects.rb
@@ -0,0 +1,11 @@
+class CreateClassSubjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :class_subjects do |t|
+      t.string :name
+      t.string :description
+      t.integer :max_num_students
+
+      t.timestamps
+    end
+  end
+end
