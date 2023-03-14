aa29e955dc770751b5e690df174d15c16301ce1a
Mahdi Aghaali <mahdi.aghaali@yahoo.com>
2023-03-14 22:07:15 +0330

    Add more course seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,10 @@ City.create(city_name: "Mexico")
 
 # data with foreign key
 Course.create(name: "english", description: "english classes", max_num_students: 20,teacher_id: Teacher.first.id)
+Course.create(name: "spanish", description: "spanish classes", max_num_students: 20,teacher_id: Teacher.first.id)
+Course.create(name: "persian", description: "persian classes", max_num_students: 20,teacher_id: Teacher.first.id)
+Course.create(name: "french", description: "french classes", max_num_students: 20,teacher_id: Teacher.first.id)
+
+
 Schedule.create(day_of_week: Date.today,start_time: Time.now,duration: Time.now,course_id: Course.first.id)
 Enrollment.create(sign_up_date: Date.today, course_id: Course.first.id,user_id: User.first.id, city_id: City.last.id)
