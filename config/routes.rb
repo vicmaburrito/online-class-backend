b524c2c17b32eaac6453d13c04c8a7d543f6319d
Mahdi Aghaali <mahdi.aghaali@yahoo.com>
2023-03-14 22:07:06 +0330

    Change route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  resources :users
   post '/auth/login', to: 'authentication#login'
-
   
   namespace :api do
     namespace :v1 do
-    resources :schedules, only: [:index, :show, :create, :update, :destroy]
-    resources :enrollments, only: [:index, :show, :create, :update, :destroy]
-    resources :courses, only: [:index, :show, :create, :update, :destroy]
-    resources :cities, only: [:index, :show, :create, :update, :destroy]
-    resources :teachers, only: [:index, :show, :create, :update, :destroy]
+      resources :users
+      resources :schedules, only: [:index, :show, :create, :update, :destroy]
+      resources :enrollments, only: [:index, :show, :create, :update, :destroy]
+      resources :courses, only: [:index, :show, :create, :update, :destroy]
+      resources :cities, only: [:index, :show, :create, :update, :destroy]
+      resources :teachers, only: [:index, :show, :create, :update, :destroy]
+    end
   end
-end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
