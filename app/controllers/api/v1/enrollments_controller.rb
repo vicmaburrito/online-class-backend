8496dc9ef60ae5d8f34eb3d7b9abebce46a82415
vicma <vmaldaraca@gmail.com>
2023-03-15 15:26:56 -0600

    Refactor enrollments query

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -3,13 +3,11 @@ class Api::V1::EnrollmentsController < ApplicationController
 
   # GET /classes or /classes.json
   def index
-    
-    sql = "SELECT * from enrollments as a
-          left join courses as b on a.course_id = b.id
-          WHERE a.user_id = #{@current_user.id}"
-    @enrollment = ActiveRecord::Base.connection.execute(sql)
-
-    render json: @enrollment
+    enrollments = Enrollment
+                   .joins(:course)
+                   .where(user_id: @current_user.id)
+                   .select('enrollments.*, courses.*')
+    render json: enrollments
   end
 
   # GET /classes/1 or /classes/1.json
