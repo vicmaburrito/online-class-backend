0d7a9e9e0ec7d5977e838bfb31588f9a0ceeaf8c
vicma <vmaldaraca@gmail.com>
2023-03-16 23:56:32 -0600

    Add City model tests

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @city = City.new(city_name: 'Mexico')
+  end
+
+  describe 'When the city model is tested' do
+    it 'validates the name of the object' do
+      expect(@city.city_name).to eq 'Mexico'
+    end
+  end
+
+  describe 'When user model wants to be save it' do
+    it 'is valid with valid attributes' do
+      expect(@city).to be_valid
+    end
+
+    it 'is not valid without an city name' do
+      city = City.new(city_name: nil)
+      expect(city).to_not be_valid
+    end
+  end
 end
